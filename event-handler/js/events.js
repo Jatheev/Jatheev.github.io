1cdaf9f5b6022f7b5aba9867f728f78f29aefc77
Jatheevraj <jatheev1005@gmail.com>
2021-10-31 04:29:14 +0800

    002

--- event-handler/js/events.js
+++ event-handler/js/events.js
@@ -2,6 +2,7 @@ function transformUpperCase(_val){
     return _val.toUpperCase();
 }
 
+
 let elNamaPengguna = document.getElementById("namaPengguna")
 elNamaPengguna.addEventListener("keyup",function(){
     elNamaPengguna.value = transformUpperCase(elNamaPengguna.value)
