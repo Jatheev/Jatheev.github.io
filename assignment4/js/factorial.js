65f38ef506fa051b57cbac3718f606971810a198
Jatheevraj <jatheev1005@gmail.com>
2021-10-31 05:53:16 +0800

    003

--- assignment4/js/factorial.js
+++ assignment4/js/factorial.js
@@ -4,5 +4,5 @@ function factorial()
     var i, f=1;
     for (i=1; i<=n; i++) f *= i;
     document.getElementById("fact").innerHTML = f;
-    alert("The factorial of entered number is") = f;
+    alert("The factorial of entered number is " + f );
 }
